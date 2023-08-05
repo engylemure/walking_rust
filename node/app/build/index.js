4ec4e52b308b82ce9d4270b590f721e3881916da
Jordão Rodrigues Oliveira Rosario <jordao.rosario01@gmail.com>
2023-08-05 14:48:43 -0300

    more adjustments

--- node/app/build/index.js
+++ node/app/build/index.js
@@ -4,12 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const express_ws_1 = __importDefault(require("express-ws"));
 const express_2 = require("express");
 const app = (0, express_1.default)();
+// Setting up WS
+const expressWsInstance = (0, express_ws_1.default)(app);
 const route = (0, express_2.Router)();
 app.use(express_1.default.json());
-route.get('/', (req, res) => {
-    res.json({ message: 'hello world with Typescript' });
+route.get("/", (req, res) => {
+    res.json({ message: "hello world with Typescript" });
 });
 app.use(route);
+expressWsInstance.app.ws('/ws', function (ws, req) {
+    ws.on('message', function (msg) {
+        console.log(msg);
+    });
+    console.log('socket', req);
+});
 app.listen(process.env.SERVER_PORT, () => console.log(`server running on port ${process.env.SERVER_PORT}`));
--- node/app/src/index.ts
+++ node/app/src/index.ts
@@ -1,18 +1,28 @@
-import express from 'express'
-
-import { Router, Request, Response } from 'express';
+import express from "express";
+import expressWS from 'express-ws';
+import { Router, Request, Response } from "express";
 
 const app = express();
+// Setting up WS
+const expressWsInstance = expressWS(app);
 
-const route = Router()
+const route = Router();
 
-app.use(express.json())
+app.use(express.json());
 
-route.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'hello world with Typescript' })
+route.get("/", (req: Request, res: Response) => {
+  res.json({ message: "hello world with Typescript" });
+});
+
+app.use(route);
+
+expressWsInstance.app.ws('/ws', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req);
 })
 
-app.use(route)
-
-
-app.listen(process.env.SERVER_PORT, () => console.log(`server running on port ${process.env.SERVER_PORT}`)) 
+app.listen(process.env.SERVER_PORT, () =>
+  console.log(`server running on port ${process.env.SERVER_PORT}`),
+);
